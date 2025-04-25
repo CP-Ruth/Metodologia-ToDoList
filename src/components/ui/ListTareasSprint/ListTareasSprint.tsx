a230f611930d3853c79d5e564109e85fbda75ce3
Ruth <elith.tati@gmail.com>
2025-04-24 21:05:40 -0300

    Funcionalidades de botones

--- src/components/ui/ListTareasSprint/ListTareasSprint.tsx
+++ src/components/ui/ListTareasSprint/ListTareasSprint.tsx
@@ -4,12 +4,19 @@ import style from "./ListTareasSprint.module.css"
 import { useSprints } from "../../../hooks/useSprints";
 import { useEffect, useState } from "react";
 import { ISprint } from "../../../types/ISprint";
-
+import { MdOutlinePlaylistAdd } from "react-icons/md";
+import { ITarea } from "../../../types/ITarea";
+import { ModalEditarAñadir } from "../Modal/ModalEditarAñadir/ModalEditarAñadir";
 
 export const ListTareasSprint = () => {
   //Leer query param sprint para filtrar y mostrar las tareas del sprint seleccionado.
   const { sprintId } = useParams();
   const { getSprintPorId } = useSprints();
+  const [openModalEdit, setOpenModalEdit] = useState(false);
+  const [selectedTarea, setSelectedTarea] = useState<ITarea | null>(null);
+
+  const handleCloseModalEA = () => { setOpenModalEdit(false) };
+
 
   const [sprint, setSprint] = useState<ISprint | null>(null);
   useEffect(() => {
@@ -26,15 +33,16 @@ export const ListTareasSprint = () => {
   return (
     <section>
       <div className={style.sprintHeader}>
-        <h2>{sprint?.nombre}</h2>
-        <button></button>
+        <h2>Tareas del {sprint?.nombre}</h2>
+        <button onClick={() => { setSelectedTarea(null); setOpenModalEdit(true); }}>Crear tarea <MdOutlinePlaylistAdd /></button>
       </div>
 
       <div className={style.sprintTablero}>
-        <TareasPorEstado sprint={sprint} tareas={sprint.tareas.filter((t) => t.estado === "pendiente")} estadoTarea="pendiente"/>
-        <TareasPorEstado sprint={sprint} tareas={sprint.tareas.filter((t) => t.estado === "en_progreso")} estadoTarea="en_progreso"/>
-        <TareasPorEstado sprint={sprint} tareas={sprint.tareas.filter((t) => t.estado === "completada")} estadoTarea="completada"/>
+        <TareasPorEstado sprint={sprint} tareas={sprint.tareas.filter((t) => t.estado === "pendiente")} estadoTarea="pendiente" />
+        <TareasPorEstado sprint={sprint} tareas={sprint.tareas.filter((t) => t.estado === "en_progreso")} estadoTarea="en_progreso" />
+        <TareasPorEstado sprint={sprint} tareas={sprint.tareas.filter((t) => t.estado === "completada")} estadoTarea="completada" />
       </div>
+      {openModalEdit && <ModalEditarAñadir type="tarea" editData={selectedTarea} handleCloseModal={handleCloseModalEA} />}
     </section>
   )
 }
--- src/components/ui/Navegacion/Navegacion.tsx
+++ src/components/ui/Navegacion/Navegacion.tsx
@@ -5,10 +5,14 @@ import { ListSprint } from '../ListSprint/ListSprint';
 import { useState } from 'react';
 import { ISprint } from '../../../types/ISprint';
 import { ModalEditarAñadir } from '../Modal/ModalEditarAñadir/ModalEditarAñadir';
+import { useNavigate } from 'react-router-dom';
 
 
 export const Navegacion = () => {
-
+    const navigate = useNavigate();
+    const verListaTareasDelBacklog = () => {
+        navigate(`/`);
+    }
     const [openModalAdd, setOpenModalAdd] = useState(false);
     const [selectedSprint, setSelectedSprint] = useState<ISprint | null>(null);
 
@@ -19,7 +23,7 @@ export const Navegacion = () => {
 
     return (
         <div className={styles.containerNav}>
-            <button className={styles.buttonBacklog} >Backlog      <FaRegEdit /></button>
+            <button className={styles.buttonBacklog}  onClick={verListaTareasDelBacklog}>Backlog      <FaRegEdit /></button>
             <div className={styles.springSpace}>
                 <div className={styles.springSpace__title}>
                 <h3>Lista de Springs</h3>
