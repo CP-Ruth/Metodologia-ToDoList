0853c738ddbc4cd858539d059974eae1a93c2f7d
Uriel Castellino <castellino.u@gmail.com>
2025-04-29 23:43:22 -0300

    arreglo del logo en header

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,10 +1,12 @@
 import styles from './Header.module.css'
-//import logo from 'src/assets/images/mateYBizcochuelo.PNG';
+import logo from '../../../assets/images/mateBizcochuelo.ico';
+
 
 export const Header = () => {
     return (
         <div className={styles.containerHeader}>
-            <img className={styles.HeaderLogo } src="src/assets/images/mateYBizcochuelo.PNG" alt="Logo" />
+            <img className={styles.HeaderLogo} src={logo} alt="Logo" />
+
             <div>
                 <h1 className={styles.HeaderTitle}>Mate y Bizcochuelo</h1>
                 <p className={styles.HeaderSubTitle}>Administrador de tareas</p>
